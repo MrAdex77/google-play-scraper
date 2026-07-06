57ec243037e396a61a4bfde4de41e7ad65c838e2
MrAdex77 <mradex77@gmail.com>
2026-07-06 14:53:58 +0200

    chore: add eslint and prettier setup

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist', 'coverage', 'node_modules', '.reference', 'test/fixtures'] },
+  js.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname },
+    },
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'no-console': 'error',
+    },
+  },
+  { files: ['**/*.js'], extends: [tseslint.configs.disableTypeChecked] },
+  {
+    files: ['**/*.test.ts', 'e2e/**'],
+    rules: { '@typescript-eslint/no-non-null-assertion': 'off' },
+  },
+  prettier,
+);
